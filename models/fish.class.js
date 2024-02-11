5bb120c3b8ca8869e4994c27915771286305c62e
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-02-11 22:15:51 +0100

    random position enemies

--- models/fish.class.js
+++ models/fish.class.js
@@ -1,5 +1,7 @@
 class Fish extends MovableObject {
     constructor() { 
         super().loadImage('../img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png') 
+        this.x = 120 + Math.random() * 500; 
+        this.y = 300;
     }
 }

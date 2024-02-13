00a7d34ab61afa722ae5d36274a105d56edc9267
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-02-13 15:54:22 +0100

    Backgrounds added

--- models/world.class.js
+++ models/world.class.js
@@ -1,25 +1,40 @@
 class World {
   character = new Character();
   enemies = [new Fish(), new Fish(), new Fish()];
-  canvas;
+  backgroundObjects = [
+    new BackgroundObject("../img/3. Background/Layers/5. Water/D1.png", 0),
+    new BackgroundObject("../img/3. Background/Layers/4.Fondo 2/D1.png", 0),
+    new BackgroundObject("../img/3. Background/Layers/3.Fondo 1/D1.png", 0),
+    new BackgroundObject("../img/3. Background/Layers/2. Floor/D1.png", 0),
+    new BackgroundObject("../img/3. Background/Layers/2. Floor/D1.png", 0),
+    new BackgroundObject("../img/3. Background/Legacy/Layers/1. Light/1.png", 0),
+  ];
   ctx;
 
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
-    this.canvas = canvas; 
+    this.canvas = canvas;
     this.draw();
   }
 
   draw() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); 
-    this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-    this.enemies.forEach(enemy => { 
-      this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-    });
-
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.addObjectsToMap(this.backgroundObjects);
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.enemies);
     let self = this;
     requestAnimationFrame(function () {
       self.draw();
     });
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((o) => {
+      this.addToMap(o);
+    });
+  }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }

88528e6cfcc556a7fc589db27f606b8e80948e18
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-02-14 20:12:20 +0100

    standing  animation

--- models/character.class.js
+++ models/character.class.js
@@ -3,8 +3,39 @@ class Character extends MovableObject {
   y = 220;
   height = 200;
   width = 300;
+  IMAGES_STANDING = [
+    "../img/1.Sharkie/1.IDLE/2.png",
+    "../img/1.Sharkie/1.IDLE/3.png",
+    "../img/1.Sharkie/1.IDLE/4.png",
+    "../img/1.Sharkie/1.IDLE/5.png",
+    "../img/1.Sharkie/1.IDLE/6.png",
+    "../img/1.Sharkie/1.IDLE/7.png",
+    "../img/1.Sharkie/1.IDLE/8.png",
+    "../img/1.Sharkie/1.IDLE/9.png",
+    "../img/1.Sharkie/1.IDLE/10.png",
+    "../img/1.Sharkie/1.IDLE/11.png",
+    "../img/1.Sharkie/1.IDLE/12.png",
+    "../img/1.Sharkie/1.IDLE/13.png",
+    "../img/1.Sharkie/1.IDLE/14.png",
+    "../img/1.Sharkie/1.IDLE/15.png",
+    "../img/1.Sharkie/1.IDLE/16.png",
+    "../img/1.Sharkie/1.IDLE/17.png",
+    "../img/1.Sharkie/1.IDLE/18.png",
+  ];
+  currentImage = 0;
+
   constructor() {
     super().loadImage("../img/1.Sharkie/1.IDLE/1.png");
+    this.loadStandingImages(this.IMAGES_STANDING);
+    this.animate();
   }
-  jump() {}
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_STANDING.length
+      let path = this.IMAGES_STANDING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 400);
+  }
 }
--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -4,11 +4,20 @@ class MovableObject {
     img;
     height = 200;
     width = 300;
+    imageCache = [];
 
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadStandingImages(arr) {
+      arr.forEach((path) => {
+        let img = new Image();
+        img.src = path;
+        this.imageCache[path] = img;
+      });
     }
 
     moveLeft() {

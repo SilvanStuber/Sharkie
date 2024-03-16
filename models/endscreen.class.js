ed2c481cf44f712bf4ea87854dbb16580d236ea2
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-03-16 20:08:07 +0100

    try again button

--- models/endscreen.class.js
+++ models/endscreen.class.js
@@ -12,9 +12,16 @@ class EndScreen extends MovableObject {
     "./img/6.Botones/Tittles/Game Over/Recurso 12.png",
     "./img/6.Botones/Tittles/Game Over/Recurso 13.png",
   ];
+  IMAGE_TRY_AGAIN = [
+    "./img/6.Botones/Try again/Recurso 15.png",
+    "./img/6.Botones/Try again/Recurso 16.png",
+    "./img/6.Botones/Try again/Recurso 17.png",
+    "./img/6.Botones/Try again/Recurso 18.png",
+  ];
 
   width = 480;
   height = 280;
+  imageTryAgainCounter = 0;
 
   constructor() {
     super();
@@ -22,8 +29,9 @@ class EndScreen extends MovableObject {
     this.loadImages(this.IMAGE_WIN);
     this.loadImages(this.IMAGE_GAMEOVER);
     this.y = 80;
-    setInterval(() => this.generateWinAnimation(), 200);
-    setInterval(() => this.generateGameOverAnimation(), 200);
+    setInterval(() => this.generateWinAnimation(), 250);
+    setInterval(() => this.generateGameOverAnimation(), 250);
+    setInterval(() => this.loadTryAgain(), 250);
   }
 
   generateWinAnimation() {
@@ -37,6 +45,14 @@ class EndScreen extends MovableObject {
     if (characterDead) {
       this.x = characterPositionX + 120;
       this.playAnimation(this.IMAGE_GAMEOVER);
+    }
+  }
+
+  loadTryAgain() {
+    if (endbossDead || characterDead) {
+      let i = this.imageTryAgainCounter % this.IMAGE_TRY_AGAIN.length;
+      document.getElementById("tryAgainButton").src = this.IMAGE_TRY_AGAIN[i];
+      this.imageTryAgainCounter++;
     }
   }
 }

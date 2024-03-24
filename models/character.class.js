3e99f1137c2e7824dcfb99251057abe928c7a428
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-03-24 13:39:46 +0100

    Control of the character via two axes simultaneously

--- models/character.class.js
+++ models/character.class.js
@@ -1,9 +1,9 @@
 /**
  * Represents a character in the game.
  * @class
- * @extends MovableObject
+ * @extends CharacterImages
  */
-class Character extends MovableObject {
+class Character extends CharacterImages {
   x = 200;
   y = 220;
   height = 200;
@@ -13,117 +13,6 @@ class Character extends MovableObject {
   offsetWidht = 10;
   offsetHeight = 130;
   speed = 2;
-  IMAGES_STANDING = [
-    "./img/1.Sharkie/1.IDLE/2.png",
-    "./img/1.Sharkie/1.IDLE/3.png",
-    "./img/1.Sharkie/1.IDLE/4.png",
-    "./img/1.Sharkie/1.IDLE/5.png",
-    "./img/1.Sharkie/1.IDLE/6.png",
-    "./img/1.Sharkie/1.IDLE/7.png",
-    "./img/1.Sharkie/1.IDLE/8.png",
-    "./img/1.Sharkie/1.IDLE/9.png",
-    "./img/1.Sharkie/1.IDLE/10.png",
-    "./img/1.Sharkie/1.IDLE/11.png",
-    "./img/1.Sharkie/1.IDLE/12.png",
-    "./img/1.Sharkie/1.IDLE/13.png",
-    "./img/1.Sharkie/1.IDLE/14.png",
-    "./img/1.Sharkie/1.IDLE/15.png",
-    "./img/1.Sharkie/1.IDLE/16.png",
-    "./img/1.Sharkie/1.IDLE/17.png",
-    "./img/1.Sharkie/1.IDLE/18.png",
-  ];
-  IMAGES_SWIM = [
-    "./img/1.Sharkie/3.Swim/1.png",
-    "./img/1.Sharkie/3.Swim/2.png",
-    "./img/1.Sharkie/3.Swim/3.png",
-    "./img/1.Sharkie/3.Swim/4.png",
-    "./img/1.Sharkie/3.Swim/5.png",
-    "./img/1.Sharkie/3.Swim/6.png",
-  ];
-  IMAGES_POISON_HURT = [
-    "./img/1.Sharkie/5.Hurt/1.Poisoned/2.png",
-    "./img/1.Sharkie/5.Hurt/1.Poisoned/3.png",
-    "./img/1.Sharkie/5.Hurt/1.Poisoned/4.png",
-    "./img/1.Sharkie/5.Hurt/1.Poisoned/5.png",
-  ];
-
-  IMAGES_ELECTRO_HURT = [
-    "./img/1.Sharkie/5.Hurt/2.Electric shock/1.png",
-    "./img/1.Sharkie/5.Hurt/2.Electric shock/2.png",
-    "./img/1.Sharkie/5.Hurt/2.Electric shock/3.png",
-  ];
-  IMAGES_POISON_DEAD = [
-    "./img/1.Sharkie/6.dead/1.Poisoned/1.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/2.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/3.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/4.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/5.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/6.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/7.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/8.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/9.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/10.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/11.png",
-    "./img/1.Sharkie/6.dead/1.Poisoned/12.png",
-  ];
-  IMAGES_ELECTRO_DEAD = [
-    "./img/1.Sharkie/6.dead/2.Electro_shock/1.png",
-    "./img/1.Sharkie/6.dead/2.Electro_shock/2.png",
-    "./img/1.Sharkie/6.dead/2.Electro_shock/3.png",
-    "./img/1.Sharkie/6.dead/2.Electro_shock/4.png",
-    "./img/1.Sharkie/6.dead/2.Electro_shock/5.png",
-    "./img/1.Sharkie/6.dead/2.Electro_shock/6.png",
-    "./img/1.Sharkie/6.dead/2.Electro_shock/7.png",
-    "./img/1.Sharkie/6.dead/2.Electro_shock/8.png",
-    "./img/1.Sharkie/6.dead/2.Electro_shock/9.png",
-    "./img/1.Sharkie/6.dead/2.Electro_shock/10.png",
-  ];
-  IMAGES_SLAP = [
-    "./img/1.Sharkie/4.Attack/Fin slap/1.png",
-    "./img/1.Sharkie/4.Attack/Fin slap/4.png",
-    "./img/1.Sharkie/4.Attack/Fin slap/5.png",
-    "./img/1.Sharkie/4.Attack/Fin slap/6.png",
-    "./img/1.Sharkie/4.Attack/Fin slap/7.png",
-    "./img/1.Sharkie/4.Attack/Fin slap/8.png",
-  ];
-  IMAGES_GOSLEEP = [
-    "./img/1.Sharkie/2.Long_IDLE/i1.png",
-    "./img/1.Sharkie/2.Long_IDLE/I2.png",
-    "./img/1.Sharkie/2.Long_IDLE/I3.png",
-    "./img/1.Sharkie/2.Long_IDLE/I4.png",
-    "./img/1.Sharkie/2.Long_IDLE/I5.png",
-    "./img/1.Sharkie/2.Long_IDLE/I6.png",
-    "./img/1.Sharkie/2.Long_IDLE/I7.png",
-    "./img/1.Sharkie/2.Long_IDLE/I8.png",
-    "./img/1.Sharkie/2.Long_IDLE/I9.png",
-  ];
-  IMAGES_SLEEP = [
-    "./img/1.Sharkie/2.Long_IDLE/I10.png",
-    "./img/1.Sharkie/2.Long_IDLE/I11.png",
-    "./img/1.Sharkie/2.Long_IDLE/I12.png",
-    "./img/1.Sharkie/2.Long_IDLE/I13.png",
-    "./img/1.Sharkie/2.Long_IDLE/I14.png",
-  ];
-  IMAGES_BLOW_UP_BUBBLE = [
-    "./img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/1.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/2.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/3.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/4.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/5.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/6.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/7.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/8.png",
-  ];
-  IMAGES_BLOW_UP_BUBBLE_POISON = [
-    "./img/1.Sharkie/4.Attack/Bubble trap/For Whale/1.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/For Whale/2.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/For Whale/3.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/For Whale/4.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/For Whale/5.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/For Whale/6.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/For Whale/7.png",
-    "./img/1.Sharkie/4.Attack/Bubble trap/For Whale/8.png",
-  ];
   playAttack;
   timeWithoutMovement = 0;
   attackImageCounter = 0;
@@ -171,23 +60,100 @@ class Character extends MovableObject {
   }
 
   /**
-   * Moves the character based on keyboard input if the character is alive and the game has started.
+   * Moves the character based on keyboard input when the character is alive and the game has started. On the X-axis and Y-axis
    */
   moveCharacter() {
     if (!this.chracterIsDead && gameHasStarted) {
-      if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-        this.moveRight();
-        this.otherDirection = false;
-      } else if (this.world.keyboard.LEFT && this.x > 0) {
-        this.moveLeft();
-        this.otherDirection = true;
-      } else if (this.world.keyboard.UP && this.y > -20) {
-        this.moveUp();
-      } else if (this.world.keyboard.DOWN && this.y < 220) {
-        this.moveDown();
+      if (this.world.keyboard.RIGHT && this.world.keyboard.UP && this.y > -20) {
+        this.generateMoveRightUp();
+      } else if (this.world.keyboard.RIGHT && this.world.keyboard.DOWN && this.y < 220) {
+        this.generateMoveRightDown();
+      } else if (this.world.keyboard.LEFT && this.world.keyboard.UP && this.y > -20) {
+        this.generateMoveLeftUp();
+      } else if (this.world.keyboard.LEFT && this.world.keyboard.DOWN && this.y < 220) {
+        this.generateMoveLeftDown();
+      } else {
+        this.movementSingleAxle();
       }
       this.world.camera_x = -this.x;
     }
+  }
+
+  /**
+   * Moves the character based on keyboard input when the character is alive and the game has started. On the X-axis or Y-axis
+   */
+  movementSingleAxle() {
+    if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+      this.generateMoveRight();
+    } else if (this.world.keyboard.LEFT && this.x > 0) {
+      this.generateMoveLeft();
+    } else if (this.world.keyboard.UP && this.y > -20) {
+      this.moveUp();
+    } else if (this.world.keyboard.DOWN && this.y < 220) {
+      this.moveDown();
+    }
+  }
+
+  /**
+   * Generates a movement to the left.
+   * It sets the direction to the "other direction" before executing the moveLeft method.
+   */
+  generateMoveLeft() {
+    this.otherDirection = true;
+    this.moveLeft();
+  }
+
+  /**
+   * Generates a movement to the right.
+   * It sets the direction to not be the "other direction" before executing the moveRight method.
+   */
+  generateMoveRight() {
+    this.otherDirection = false;
+    this.moveRight();
+  }
+
+  /**
+   * Generates a combined movement to the right and upwards.
+   * Initially, it sets the direction to not be the "other direction", then
+   * executes a single axle movement, followed by an upward movement.
+   */
+  generateMoveRightUp() {
+    this.otherDirection = false;
+    this.movementSingleAxle();
+    this.moveUp();
+  }
+
+  /**
+   * Generates a movement diagonally to the right and downwards.
+   * Sets the direction to not be the "other direction," initiates single axle movement,
+   * and then moves downwards.
+   */
+  generateMoveRightDown() {
+    this.otherDirection = false;
+    this.movementSingleAxle();
+    this.moveDown();
+  }
+
+  /**
+   * Generates an upward movement towards the left.
+   * It sets the direction to the "other direction", applies single axle movement adjustments,
+   * and then executes the moveUp method to perform the upward movement.
+   */
+  generateMoveLeftUp() {
+    this.otherDirection = true;
+    this.movementSingleAxle();
+    this.moveUp();
+  }
+
+  /**
+   * Generates a downward movement towards the left.
+   * It sets the direction to the "other direction", applies single axle movement adjustments,
+   * and then executes the moveDown method to perform the downward movement.
+   */
+  generateMoveLeftDown() {
+    this.otherDirection = true;
+    this.movementSingleAxle();
+    this.moveDown();
   }
 
   /**
